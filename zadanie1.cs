13240d87166e880f00001c3600f95f72bb105de6
danyatekunov <danyatekunov@gmail.com>
2023-01-05 22:18:11 +0300

    v 1.0

---
+++ zadanie1.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое целое число");
+    int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("Первое число больше, чем второе");
+
+}
+
+else
+Console.WriteLine("Второе число больше, чем первое");
