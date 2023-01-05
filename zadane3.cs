e5554288a94afda9b2475729d35a8dae3707f654
danyatekunov <121130644+danyatekunov@users.noreply.github.com>
2023-01-05 22:46:12 +0300

    Add files via upload

---
+++ zadane3.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+if (x % 2 ==0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+Console.WriteLine("Число нечётное");
