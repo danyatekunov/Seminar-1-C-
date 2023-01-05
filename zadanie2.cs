a1c3ea707a2e9487e9e9469071e1381de5516e61
danyatekunov <121130644+danyatekunov@users.noreply.github.com>
2023-01-05 22:33:18 +0300

    Add files via upload

---
+++ zadanie2.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int z = Convert.ToInt32(Console.ReadLine());
+
+if (x>y && x>z)
+
+   {
+    Console.WriteLine("Первое число наибольшее");
+   }
+
+   if (y>x && y>z)
+
+   {
+    Console.WriteLine("Второе число наибольшее");
+   }
+
+   if (z>y && z>x)
+
+   {
+    Console.WriteLine("Первое число наибольшее");
+   }
